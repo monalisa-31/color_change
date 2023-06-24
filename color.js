0d29cf57883851dc2883691cac54d8a03a9cd448
root <monalisa21@navgurukul.org>
2023-06-24 14:37:37 +0530

    first commit

---
+++ color.js
@@ -0,0 +1,24 @@
+let purple_color=document.getElementById("tap1");
+purple_color.onclick=function(){
+    row.style.backgroundColor="purple";
+};
+let green_color=document.getElementById("tap2");
+green_color.onclick=function(){
+    row.style.backgroundColor="green";
+};
+let blue_color=document.getElementById("tap3");
+blue_color.onclick=function(){
+    row.style.backgroundColor="blue";
+};
+let pink_color=document.getElementById("tap4");
+pink_color.onclick=function(){
+    row.style.backgroundColor="pink";
+};
+let yellow_color=document.getElementById("tap5");
+yellow_color.onclick=function(){
+    row.style.backgroundColor="yellow";
+};
+let red_color=document.getElementById("tap6");
+red_color.onclick=function(){
+    row.style.backgroundColor="red";
+};
